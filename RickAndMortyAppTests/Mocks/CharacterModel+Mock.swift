45e199eec21bda24372577a1baa70139b5aa50ad
Nebraska Melendez <nebraskam23@gmail.com>
2022-07-19 22:37:32 -0400

    refactor test

--- RickAndMortyAppTests/Mocks/CharacterModel+Mock.swift
+++ RickAndMortyAppTests/Mocks/CharacterModel+Mock.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+@testable import RickAndMortyApp
+
+extension CharacterModel {
+    
+    static var characterMock = CharacterModel(
+        id: 0,
+        name: "Morty",
+        status: "Alive",
+        species: "Human",
+        type: "-",
+        gender: "-",
+        created: "-",
+        image: "",
+        origin: .init(name: "earth"),
+        location: .init(name: "earth")
+    )
+
+}
--- RickAndMortyAppTests/RouterTests/RouterTests.swift
+++ RickAndMortyAppTests/RouterTests/RouterTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import RickAndMortyApp
 
 class RouterTests: XCTestCase {
-
+    
     func test_has_set_rootViewController_when_start_method_is_called() throws {
         let window = UIWindow(frame: .zero)
         let sut = AppRouter(window: window)
@@ -17,6 +17,14 @@ class RouterTests: XCTestCase {
         sut.start()
         
         XCTAssertNotNil(window.rootViewController)
+
+        guard let navigationController = window.rootViewController as? UINavigationController,
+              let topController = navigationController.viewControllers.last else {
+            XCTFail()
+            return
+        }
+        let topIsAHomeController = topController is HomeViewController
+        XCTAssertTrue(topIsAHomeController)
     }
 }
 
